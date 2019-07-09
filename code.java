e85e4aaaaa8ac86fb643d6800aac2b61543d2399
Anuraag <anuraag@thoughtclan.com>
2019-07-09 12:08:41 +0530

    add method 'printRed()' to print the String 'RED', in code.java

--- code.java
+++ code.java
@@ -1,7 +1,12 @@
 import java.util.*;
 
 public class code {
+	
+	public static String printRed(){
+		return "RED";
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println(printRed());
 	}
 }
