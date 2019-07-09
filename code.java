91401c80136562b4a1d578db1e76e9f01225b81d
kapil patidar <50325128+kapilpatidar007@users.noreply.github.com>
2019-07-09 12:09:08 +0530

    Add a method to print String 'BLACK'

--- code.java
+++ code.java
@@ -1,7 +1,12 @@
 import java.util.*;
 
 public class code {
+	public static String printBlack(){
+		return "Black";
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println(printBlack());
 	}
 }
