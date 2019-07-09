19fa8c5974d9fea69dae9a7ded40c92d35f69647
anuraagtc <anuraag@thoughtclan.com>
2019-07-09 13:43:01 +0530

    Code to print 'RED'

--- code.java
+++ code.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class code {
 	
 	public static String printRed(){
-		return "RED";
+		return "the path of righteous man is beset on all sides by the inequities of the selfish and the tyranny of evil men";
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
