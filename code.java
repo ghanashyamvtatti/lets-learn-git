15559e8a92f71dec0996ebf5e4d56b49c24c8581
Sindhu <sindhushash97@gmail.com>
2019-07-09 12:37:07 +0530

    add function printPurple()

--- code.java
+++ code.java
@@ -4,4 +4,7 @@ public class code {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
+	public static void printPurple(){
+		System.out.println("dibs on purple hehehe");
+	}
 }
