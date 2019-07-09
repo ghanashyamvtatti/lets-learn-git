663b4f54b9537050c4a4301d7c4b31d5c96b5c81
karthik146 <karthik.c@thoughtclan.com>
2019-07-09 12:14:26 +0530

    add method to print blue

--- code.java
+++ code.java
@@ -3,5 +3,9 @@ import java.util.*;
 public class code {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		printBlue();
+	}
+	public static void printBlue(){
+		System.out.println("BLUE");
 	}
 }
